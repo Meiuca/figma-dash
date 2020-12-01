65babcf4783aae98599073ac714c75ef3a718207
levi-pires <waltvy.comercial@gmail.com>
2020-12-01 17:23:15 -0300

    update

--- src/initializer.js
+++ src/initializer.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const configHandler = require("figma-dash-core/config-handler");
-const pathToSrc = path.resolve(__dirname, "../defaults/jota-web.config.js");
+const pathToSrc = path.resolve(__dirname, "../defaults/figma-dash.config.js");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const { existsSync, copyFileSync } = require("fs");
